b6a4e064314ed03fbb142cab1e4f27e1dc614c8b
Max <max.tarasuik@gmail.com>
2020-06-10 19:52:34 +1000

    Added ability to specify time ranges for ABC client search

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ Originally designed for Triple J - thus the name.
 ### ABC
 
 Any ABC (Australian Broadcasting Corporation) radio station (TrippleJ, DoubleJ etc)
+Known ABC stations are: jazz,dig,doublej,unearthed,country,triplej,classic
+You can choose what timeframe to create a playlist from, it defaults to the previous aired standard morning and afternoon sessions 
 
 ## Setup
 
@@ -16,7 +18,7 @@ Any ABC (Australian Broadcasting Corporation) radio station (TrippleJ, DoubleJ e
 
 ```
 # Clone the repo
-git clone git@github.com:maxtarasuik/Spotijjjy.git
+git clone git@github.com:maxtara/Spotijjjy.git
 cd Spotijjjy
 
 # Create a [virtualenv](https://virtualenv.pypa.io/en/stable/)
@@ -47,8 +49,18 @@ If storing the oauth token in DynamoDB, the key to store it is 'spotijjjy_token'
 ```
 usage: python -m spotijjjy.main config.json station_id playlist_id <STORE>
 
-e.g.: python -m spotijjjy.main config.json doublej 2jhfs98s3finafzgfse9u3 file:foo.txt
-e.g.: python -m spotijjjy.main config.json triplej sfiujh38f9hs983fjs3fj9 dydb:tablename
+e.g.: python -m spotijjjy.main config.json doublej 2jhfs98s3finafzgfse9u3 file:foo.txt [ranges]
+e.g.: python -m spotijjjy.main config.json triplej sfiujh38f9hs983fjs3fj9 dydb:tablename [ranges]
+
+Optional argument ranges in the format - comma seperated list of from%to in iso format.
+e.g. 2020-01-01T12:00:00%2020-01-01T13:00:00,2020-02-01T12:00:00%2020-02-01T13:00:00
+turns into 
+    from - 2020-01-01T12:00:00
+    to   - 2020-01-01T13:00:00 
+and from - 2020-02-01T12:00:00
+    to   - 2020-02-01T13:00:00
+    
+
 
 ```
 
@@ -61,3 +73,7 @@ sls deploy
 ```
 
 Arguments are passed in a env variables, which you can put in the serverless.yml, or a secrets.json like the yaml provided.
+
+## Notes
+### Spotify searching
+I do my best to find the correct song via the spotify search api, using a variety of methods. I use Multiple API options (searching with the artist, or artists, or full text search), and from the returned search results, I try to find the exact song using fuzzy string matching. YMMV
--- spotijjjy/main.py
+++ spotijjjy/main.py
@@ -13,18 +13,26 @@ def main(event, arg2):
         config = json.loads(open(event['CONFIG_FILE']).read())  # Open and read config file
         station_id = event['STATION_ID']
         playlist_id = event['PLAYLIST_ID']
-        store = event['STORE'] # file:filename.txt or ddb
+        store = event['STORE']  # file:filename.txt or ddb
+        ranges = event['RANGES'] if "RANGES" in event else None
     elif 'CONFIG_FILE' in os.environ: #
         config = json.loads(open(os.environ['CONFIG_FILE']).read())
         station_id = os.environ['STATION_ID']
         playlist_id = os.environ['PLAYLIST_ID']
-        store = os.environ['STORE'] # file:filename.txt or ddb
+        store = os.environ['STORE']  # file:filename.txt or ddb
+        ranges = os.environ['RANGES'] if "RANGES" in os.environ else None
     else:
         raise("Config not specified in enviroment or arguments (did you forget to add the envirment variable in AWS?)")
     print("config-" + str(config))
     print("station_id-" + str(station_id))
+    try:
+        ranges = [(x.split("%")[0], x.split("%")[1]) for x in ranges.split(",")]
+    except:
+        print("ERROR -- incorrect RANGES format, should be comma seperated list of from%to in iso format")
+        print("e.g., 2020-01-01T12:00:00%2020-01-01T13:00:00,2020-02-01T12:00:00%2020-02-01T13:00:00  ")
+        exit(1)
     # Connect to ABC
-    abc = ABCClient(ranges=None, station_id=station_id)
+    abc = ABCClient(ranges=ranges, station_id=station_id)
     # Get Song Pairs
     song_pairs = abc.get_songs_for_urls()
     # print(song_pairs)
@@ -44,8 +52,15 @@ def main(event, arg2):
 
 if __name__ == "__main__":
     event = {}
-    event["CONFIG_FILE"] = sys.argv[1]
-    event['STATION_ID'] = sys.argv[2]
-    event['PLAYLIST_ID'] = sys.argv[3]
-    event['STORE'] = sys.argv[4]
-    main(event, "")
+
+    if len(sys.argv) > 1:
+        event["CONFIG_FILE"] = sys.argv[1]
+        event['STATION_ID'] = sys.argv[2]
+        event['PLAYLIST_ID'] = sys.argv[3]
+        event['STORE'] = sys.argv[4]
+        if len(sys.argv) > 5:
+            event['RANGES'] = sys.argv[5]
+        main(event, "")
+    else:
+        print("Usage:  cmd <config> <station_id> <playlist_id> <store> [ranges]")
+        print("\tSee README.me for instructions")
