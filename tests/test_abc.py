7192c44ec8b5db3b061cd479693fff9f936da4f9
Max <max.tarasuik@gmail.com>
2020-06-10 20:22:23 +1000

    Added testing framework skeleton

---
+++ tests/test_abc.py
@@ -0,0 +1,12 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+from spotijjjy import ABCClient
+
+
+abc = ABCClient(None, "triplej")
+
+def test_date_ranges():
+    v = abc.get_songs_for_urls()
+    print(v)
+    assert len(v) > 20
---
+++ tests/test_spotify.py
@@ -0,0 +1,8 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+from spotijjjy import SpotifyOathDynamoDBStore, SpotifyPlaylistUpdater
+
+def test_oath():
+    pass
+
